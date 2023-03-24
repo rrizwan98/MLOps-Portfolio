d272f5e409c9c6ec4ffcc25df12afd34af3ac0b3
email <rrizwan1998@gmail.com>
2023-03-24 15:21:33 +0500

    Data Transformation is Done

--- MLOps projects 00/src/components/data_ingestion.py
+++ MLOps projects 00/src/components/data_ingestion.py
@@ -4,6 +4,9 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -21,7 +24,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('data\Heart Attack Data Set.csv')
+            df = pd.read_csv('data/Heart Attack Data Set.csv')
             logging.info('Read the data as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -47,5 +50,8 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__ =="__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
